566065c1b4646c9f6764b9901949ad1a39f1169d
Mike Smith <mls40@cam.ac.uk>
2010-07-20 15:49:29 +0000

    Modified readCompressedData() to improve the speed.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/BeadDataPackR@48246 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/scanBabFile.R
+++ R/scanBabFile.R
@@ -11,41 +11,76 @@ readCompressedData <- function(inputFile, path = ".", probeIDs = NULL)
     ## read the file header
     header <- readHeader(con);
 
-    outputTmp <- list();
+	outputCount <- 0;
 
+	## scan through once to find and count the beads we're interested in
     for(i in 1:header$nProbeIDs) {
 
-        probeID <- readBin(con, integer(), size = 4);
-        nbeads <- readBin(con, integer(), size = 4);
-        
-        if(!is.null(probeIDs))
-            probeIDs <- probeIDs[which(probeIDs >= probeID)];
+		storeTmp <- readBin(con, integer(), size = 4, n = 2);
+		probeID <- storeTmp[1];
+		nbeads <- storeTmp[2];
         
         ## are we looking for this probeID?
         if( (is.null(probeIDs)) | (probeID %in% probeIDs) ) {
-            outputTmp[[paste(probeID)]] <- matrix(ncol = 3+(4^header$twoChannel), nrow = nbeads);
-            
-            outputTmp[[paste(probeID)]][,1] <- rep(probeID, nbeads);     
+            outputCount <- outputCount + nbeads;
+        }
+        ## How many bytes can we now skip?
+        inten <- as.logical(probeID) * 2^header$twoChannel * ( (2 * nbeads) + ( ( (nbeads - 1)  %/% 4) + 1) )
+        coords <- (2 * header$nBytes * nbeads) - (0^(!header$useOffset) * 2 * nbeads);
+        index <- 3^header$indexingMethod * nbeads;
+        seek(con = con, where = sum(inten, coords, index), origin = "current");
+    }
+
+	## reset to the beginning of the file
+	seek(con = con, where = 0, origin = "start");
+	readHeader(con);
+
+	## create output matrix and position counter
+    output <- matrix(NA, ncol = 3 + (4^header$twoChannel), nrow = outputCount);
+	pos <- 1;
+
+    for(i in 1:header$nProbeIDs) {
+
+		storeTmp <- readBin(con, integer(), size = 4, n = 2);
+		probeID <- storeTmp[1];
+		nbeads <- storeTmp[2];
+
+        if(!is.null(probeIDs))
+            probeIDs <- probeIDs[which(probeIDs >= probeID)];
+
+		## if we've already got all the requested probes, quit the loop
+		if(!is.null(probeIDs) & !length(probeIDs))
+			break;
+        
+        ## are we looking for this probeID?
+        if( (is.null(probeIDs)) | (probeID %in% probeIDs) ) {
+
+			posEnd <- pos + nbeads - 1;
+
+            output[pos:posEnd,1] <- rep(probeID, nbeads);     
             ## only read the intensities if they are there
             if(probeID) {  
-                outputTmp[[paste(probeID)]][,2] <- readIntensities(con, nbead = nbeads);
+                output[pos:posEnd,2] <- readIntensities(con, nbead = nbeads);
                 if(header$twoChannel)
-                    outputTmp[[paste(probeID)]][,5] <- readIntensities(con, nbead = nbeads);
+                    outputTmp[pos:posEnd,5] <- readIntensities(con, nbead = nbeads);
             } ## intensities not stored for probeID 0
             else {
-                outputTmp[[paste(probeID)]][,2] <- rep(0, nbeads);
+                output[pos:posEnd,2] <- rep(0, nbeads);
                 if(header$twoChannel) 
-                    outputTmp[[paste(probeID)]][,5] <- rep(0, nbeads);
+                    outputTmp[pos:posEnd,5] <- rep(0, nbeads);
             }
                 
             coords <- readCoordinates(con = con, nbeads = nbeads, nBytes = header$nBytes, twoChannel = header$twoChannel, offset = header$useOffset, base2 = header$base2)
 
-            outputTmp[[paste(probeID)]][,3:4] <- coords[1:(2*nbeads)];
+            output[pos:posEnd,3:4] <- coords[1:(2*nbeads)];
             if(header$twoChannel)
-                outputTmp[[paste(probeID)]][,6:7] <- coords[(2*nbeads+1):length(coords)];
+                output[pos:posEnd,6:7] <- coords[(2*nbeads+1):length(coords)];
                 
             ## skip the locs file index, we don't need it here
             seek(con = con, where = nbeads * 3^header$indexingMethod, origin = "current");
+
+			## increase the position counter
+			pos <- pos + nbeads;
         }
         else { ## We don't want this probe
             ## How many bytes can we skip?
@@ -58,16 +93,8 @@ readCompressedData <- function(inputFile, path = ".", probeIDs = NULL)
     ## close the bab file
     close(con);
 
-    if(length(outputTmp)) {
-        output <- matrix(ncol = ncol(outputTmp[[1]]), nrow = sum(unlist(lapply(outputTmp, nrow))));
-        pos <- 1;
-        for(i in 1:length(outputTmp)) {
-            posEnd <- pos+nrow(outputTmp[[i]])-1;
-            output[pos:posEnd,] <- outputTmp[[i]];
-            pos <- posEnd+1;
-        }
-    }
-    else { ## tell the user if no probe IDs matched
+    if(!nrow(output)) {
+		## tell the user if no probe IDs matched
         message("No matching probe IDs");
         output <- NULL;
     }
