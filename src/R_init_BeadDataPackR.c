d790d4396b28c9f260fa33e5ae17f336b7841d7a
Mike Smith <grimbough@gmail.com>
2018-02-09 09:59:16 +0100

    Use temp directory for output in vignette

--- src/R_init_BeadDataPackR.c
+++ src/R_init_BeadDataPackR.c
@@ -16,4 +16,5 @@ static const R_CallMethodDef callMethods[] =
 void R_init_BeadDataPackR(DllInfo *info)
 {
     R_registerRoutines(info, NULL, callMethods, NULL, NULL);
+    R_useDynamicSymbols(info, TRUE);
 }
